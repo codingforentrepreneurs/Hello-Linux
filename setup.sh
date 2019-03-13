40ce2bb82bdd24b0b29d2d3e5708c43e32c3c97b
Justin Mitchel <hello@teamcfe.com>
2019-03-12 22:58:55 -0700

    Update setup.sh for database creation

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+echo
+echo
+echo
 echo "What is your project name?"
 read projectname
 
@@ -7,42 +10,81 @@ projectslug="$(echo -n "${projectname}" | sed -e 's/[^[:alnum:]]/_/g' \
 
 projectgit=${projectslug}.git
 localip=$(hostname  -I | cut -f1 -d' ')
+projectDB="${projectslug}_db"
+projectDBuser="${projectslug}_user"
 
 
+echo
+echo "Your project name will be used as:"
+echo
+echo "Name: ${projectslug}"
+echo "IP: ${localip}"
+echo
+echo "If you create a git repo below, you'll have:"
+echo "Repo: /var/repo/${projectgit}/"
+echo "Working Dir: /var/www/${projectslug}/"
+echo "Working Dir Symlink: ~/${projectslug}"
+echo "Remote: ssh://${USER}@${localip}/var/repo/${projectgit}"
+echo 
+echo "If you create a database below, you'll have:"
+echo "Database Name: ${projectDB}"
+echo "Database Username: ${projectDBuser}"
+echo "Database Password: <you set below>"
+echo
+echo "These values will be generated automatically based the name you gave."
 
-echo
-echo "Your project name will be stored as:"
-echo
-echo "${projectslug} on ${localip}"
-echo "git repo on /var/repo/${projectgit}/"
-echo "Working dir /var/www/${projectslug}/"
-echo
-echo "A shortcut will be created on ~/${projectslug}"
-echo
-echo
 
 echo "Do you want to continue? y/n"
 read runSetup
-runSetupResponse="$(echo -n "${doSetup}" | sed -e 's/[^[:alnum:]]/-/g' \
+runSetupResponse="$(echo -n "${runSetup}" | sed -e 's/[^[:alnum:]]/-/g' \
 | tr -s '_' | tr A-Z a-z)"
 
 if [ "$runSetupResponse" == "n" ]
-then
-    exit 1
+    then 
+        echo 'Setup exited.' 
+        exit
 fi
 
-
-echo "Run system setup installs? y/n (recommended)"
-read doSetup
-doSetupResponse="$(echo -n "${doSetup}" | sed -e 's/[^[:alnum:]]/-/g' \
+echo "Run latest installs? y/n"
+read doInstalls
+doInstallsResponse="$(echo -n "${doInstalls}" | sed -e 's/[^[:alnum:]]/-/g' \
 | tr -s '_' | tr A-Z a-z)"
 
 
-if [ "$doSetupResponse" == "y" ]
+echo "Setup production-ready git repo for project? y/n (recommended)"
+read createGitRepo
+createGitRepoResponse="$(echo -n "${createGitRepo}" | sed -e 's/[^[:alnum:]]/-/g' \
+| tr -s '_' | tr A-Z a-z)"
+
+
+echo "Do you want to create a database? (y/n)"
+read databaseCreate
+databaseCreateResponse="$(echo -n "${databaseCreate}" | sed -e 's/[^[:alnum:]]/-/g' \
+| tr -s '_' | tr A-Z a-z)"
+
+if [ "$databaseCreateResponse" == 'y' ]
 then
+    while true; do
+        echo "What would you like the database password to be?"
+        read -s -p "Password (typing hidden): " newPassword
+        echo
+        read -s -p "Confirm password (typing hidden): " password2
+        echo
+        [ "$newPassword" = "$password2" ] && break
+        echo "Please try again"
+    done
+fi
+
+
+
+if [ "$doInstallsResponse" == "y" ]
+then
+    echo 'Installing dependancies'
+    echo
+    echo
     sudo apt-get update -y
 
-    sudo apt-get install build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip -y
+    sudo apt-get install build-essential libssl-dev libpq-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip -y
 
     sudo apt-get install supervisor -y 
 
@@ -54,6 +96,8 @@ then
 
     sudo apt-get install postgresql postgresql-contrib -y
 
+    sudo apt-get install nginx curl -y
+
     sudo python3 -m pip install pipenv
 
     sudo python3 -m pip install virtualenv
@@ -63,41 +107,96 @@ then
     sudo apt autoremove -y
 else:
     echo "Setup installs skipped"
+    echo
+fi
+
+
+if [ "$createGitRepoResponse" == "y" ]
+then
+    echo 'Creating git repo...'
+    echo
+    echo
+    sudo mkdir /var/www/
+    sudo mkdir /var/www/${projectslug}/
+    cd ~/
+    ln -s /var/www/${projectslug}/ ~/
+
+
+    mkdir /var/repo/
+    mkdir /var/repo/${projectgit}/
+    cd /var/repo/${projectgit}/
+
+    git init --bare
+
+    cat <<EOT >> /var/repo/${projectgit}/hooks/post-receive
+git --work-tree=/var/www/${projectslug} --git-dir=/var/repo/${projectgit} checkout -f
+EOT
+
+    chmod +x /var/repo/${projectgit}/hooks/post-receive
+
+    echo "Success. You can access your project on your server at:"
+    echo
+    echo "Be sure to run this on your local repo:"
+    echo "git remote add live ssh://${USER}@${localip}/var/repo/${projectgit}"
+fi
+
+if [ "$databaseCreateResponse" == 'y' ]
+then
+    echo
+    echo 'Creating postgresql database.'
+    echo
+    echo "Create Root postgres user (ignore if fails)"
+    sudo -u postgres createuser $USER
+
+    psql --command="CREATE DATABASE ${projectDB};"
+
+    psql --command="CREATE USER ${projectDBuser} WITH PASSWORD '${newPassword}';"
+
+    psql --command="ALTER ROLE ${projectDBuser} SET client_encoding TO 'utf8';"
+
+    psql --command="ALTER ROLE ${projectDBuser} SET default_transaction_isolation TO 'read committed';"
+
+    psql --command="ALTER ROLE ${projectDBuser} SET timezone TO 'UTC';"
+
+    psql --command="GRANT ALL PRIVILEGES ON DATABASE ${projectDB} TO ${projectDBuser};"
+
+    echo "Database created"
+    echo "Database name: ${projectDB}"
+    echo "Database username ${projectDBuser}"
+    echo "Database password ******** (set above)"
 fi
 
 
 
-sudo mkdir /var/www/
-sudo mkdir /var/www/${projectslug}/
-cd ~/
-ln -s /var/www/${projectslug}/ ~/
 
-
-mkdir /var/repo/
-mkdir /var/repo/${projectgit}/
-cd /var/repo/${projectgit}/
-
-git init --bare
-
-cat <<EOT >> /var/repo/${projectgit}/hooks/post-receive
-git --work-tree=/var/www/${projectslug} --git-dir=/var/repo/${projectgit} checkout -f
-EOT
-
-chmod +x /var/repo/${projectgit}/hooks/post-receive
-
-
-
-
-
-echo "Success. You can access your project on your server at:"
-echo "/var/www/${projectslug}"
-echo
-echo "Your project is linked"
-echo "~/${projectslug}"
 echo
 echo
-echo "Be sure to run this on your local repo:"
-echo "git remote add live ssh://${USER}@${localip}/var/repo/${projectgit}"
 echo
+echo "//////// Final Summary \\\\\\\\\\\\\\\\"
+echo
+echo
+if [ "$databaseCreateResponse" == 'y' ]
+then
+    echo "**Postgresql Database Created**"
+    echo
+    echo "Database name: ${projectDB}"
+    echo "Database username ${projectDBuser}"
+    echo "Database password ******** (set above)"
+fi
+if [ "$createGitRepoResponse" == "y" ]
+then
+    echo
+    echo "**Repo & Project Created**"
+    echo
+    echo "Your project is stored in"
+    echo "/var/www/${projectslug}"
+    echo "~/${projectslug}"
+    echo
+    echo "Update your local git repo to..."
+    echo 
+    echo "git remote add live ssh://${USER}@${localip}/var/repo/${projectgit}"
+    echo
+fi
+
 
 
