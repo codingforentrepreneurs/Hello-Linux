09a2f09200d511d7900d3e44f9b7d2a4f31aab86
Justin Mitchel <hello@teamcfe.com>
2019-03-13 10:32:52 -0700

    Added database settings

--- src/cfehome/settings/production.py
+++ src/cfehome/settings/production.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hello_linux_db',
+        'USER':  'hello_linux_user',
+        'PASSWORD': '8d41d0ac-a4bc-4873-b2c6-ba19d5d4adeb',
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
